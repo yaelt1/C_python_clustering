10dfaaa757e94e0ef16fe6acb82aacb5708a1fa9
Yael Tzur <116788451+yaelt1@users.noreply.github.com>
2023-06-23 15:40:14 +0300

    Delete new_hw2 directory

--- new_hw2/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import Extension, setup
-
-module = Extension("mykmeanssp", sources=['kmeansmodule.c', 'kmeans.c'])
-setup(name='mykmeanssp',
-     version='1.0',
-     description='Python wrapper for custom C extension',
-     ext_modules=[module])
